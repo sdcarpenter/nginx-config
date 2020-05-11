b4bccbadd18bad10c2cd1a9511e01dbfdd3d5025
Ubuntu <ubuntu@ip-172-26-1-16.us-west-2.compute.internal>
2020-05-11 22:59:50 +0000

    Add missing shell scripts

---
+++ restore-volume.sh
@@ -0,0 +1,10 @@
+VOLUME=
+
+case "$1" in
+    nginx|certbot-etc) VOLUME=$1;;
+    *) echo "Unknown container"; exit 1 ;;
+esac
+
+docker run --rm -i \
+  -v nginxconfig_$VOLUME:/target \
+  busybox tar -xzC /target < $VOLUME-backup.tar.gz
