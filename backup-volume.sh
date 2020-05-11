8c67419638680414b44cf4414d060a730ec31a6b
Scott Carpenter <sdcarpenter@gmail.com>
2020-05-11 22:59:50 +0000

    Add missing shell scripts

---
+++ backup-volume.sh
@@ -0,0 +1,10 @@
+VOLUME=
+
+case "$1" in
+    nginx|certbot-etc) VOLUME=$1;;
+    *) echo "Unknown container"; exit 1 ;;
+esac
+
+docker run --rm \
+    -v nginxconfig_$VOLUME:/source:ro \
+    busybox tar -czC /source . > $VOLUME-backup.tar.gz
