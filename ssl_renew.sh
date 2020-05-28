cc214838903150149fcfc395bea1ef8d6ace636d
Scott Carpenter <sdcarpenter@gmail.com>
2020-05-28 03:14:16 +0000

    Restore auto renew

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 COMPOSE="/usr/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
-exit
 cd /home/ubuntu/src/nginx-config
 $COMPOSE run certbot-hoa renew && $COMPOSE kill -s SIGHUP webserver
 $COMPOSE run certbot-scottdcarpenter renew && $COMPOSE kill -s SIGHUP webserver
