d5f9c9db65cd7257a84eb422c381bd6fc31c8864
Scott Carpenter <sdcarpenter@gmail.com>
2021-04-21 17:51:23 +0000

    Fix up path in SSL renewal script

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COMPOSE="/usr/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
-cd /home/ubuntu/src/nginx-config
+cd /home/ubuntu/src/nginxconfig
 $COMPOSE run certbot-hoa renew && $COMPOSE kill -s SIGHUP nginx
 $COMPOSE run certbot-scottdcarpenter renew && $COMPOSE kill -s SIGHUP nginx
 $DOCKER system prune -af
